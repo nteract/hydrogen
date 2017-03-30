d16060248ac3abe87a428ffb8c2f2c09e5aabdfa
Lukas Geiger <lukas.geiger94@gmail.com>
2017-03-30 16:40:57 +0200

    Use it in specs to get better error messages

--- spec/utils-spec.js
+++ spec/utils-spec.js
@@ -10,55 +10,57 @@ import {
   getEmbeddedScope
 } from "./../lib/utils";
 
-describe("grammarToLanguage", () => {
-  expect(grammarToLanguage({ name: "Kernel Name" })).toEqual("kernel name");
-  expect(grammarToLanguage(null)).toBeNull();
-  expect(grammarToLanguage(undefined)).toBeNull();
-});
+describe("utils", () => {
+  it("grammarToLanguage", () => {
+    expect(grammarToLanguage({ name: "Kernel Name" })).toEqual("kernel name");
+    expect(grammarToLanguage(null)).toBeNull();
+    expect(grammarToLanguage(undefined)).toBeNull();
+  });
 
-describe("reactFactory", () => {
-  const compDisposable = new CompositeDisposable();
-  spyOn(compDisposable, "add").andCallThrough();
-  spyOn(ReactDOM, "render");
-  spyOn(ReactDOM, "unmountComponentAtNode");
-  const teardown = jasmine.createSpy("teardown");
+  it("reactFactory", () => {
+    const compDisposable = new CompositeDisposable();
+    spyOn(compDisposable, "add").andCallThrough();
+    spyOn(ReactDOM, "render");
+    spyOn(ReactDOM, "unmountComponentAtNode");
+    const teardown = jasmine.createSpy("teardown");
 
-  reactFactory("reactElement", "domElement", teardown, compDisposable);
+    reactFactory("reactElement", "domElement", teardown, compDisposable);
 
-  expect(ReactDOM.render).toHaveBeenCalledWith("reactElement", "domElement");
-  expect(compDisposable.add).toHaveBeenCalled();
+    expect(ReactDOM.render).toHaveBeenCalledWith("reactElement", "domElement");
+    expect(compDisposable.add).toHaveBeenCalled();
 
-  expect(ReactDOM.unmountComponentAtNode).not.toHaveBeenCalled();
-  expect(teardown).not.toHaveBeenCalled();
-  compDisposable.dispose();
-  expect(ReactDOM.unmountComponentAtNode).toHaveBeenCalledWith("domElement");
-  expect(teardown).toHaveBeenCalled();
-});
+    expect(ReactDOM.unmountComponentAtNode).not.toHaveBeenCalled();
+    expect(teardown).not.toHaveBeenCalled();
+    compDisposable.dispose();
+    expect(ReactDOM.unmountComponentAtNode).toHaveBeenCalledWith("domElement");
+    expect(teardown).toHaveBeenCalled();
+  });
 
-describe("isMultilanguageGrammar", () => {
-  expect(
-    isMultilanguageGrammar(atom.workspace.buildTextEditor().getGrammar())
-  ).toBe(false);
-  expect(isMultilanguageGrammar({ scopeName: "source.gfm" })).toBe(true);
-  expect(isMultilanguageGrammar({ scopeName: "source.asciidoc" })).toBe(true);
-});
+  it("isMultilanguageGrammar", () => {
+    expect(
+      isMultilanguageGrammar(atom.workspace.buildTextEditor().getGrammar())
+    ).toBe(false);
+    expect(isMultilanguageGrammar({ scopeName: "source.gfm" })).toBe(true);
+    expect(isMultilanguageGrammar({ scopeName: "source.asciidoc" })).toBe(true);
+  });
 
-describe("getEmbeddedScope", () => {
-  const editor = {
-    scopeDescriptorForBufferPosition: () => {
-      return {
-        getScopesArray: () => [
-          "text.md",
-          "fenced.code.md",
-          "source.embedded.python"
-        ]
-      };
-    }
-  };
-  spyOn(editor, "scopeDescriptorForBufferPosition").andCallThrough();
-  const scope = getEmbeddedScope(editor, "position");
-  expect(scope).toEqual("source.embedded.python");
-  expect(editor.scopeDescriptorForBufferPosition).toHaveBeenCalledWith(
-    "position"
-  );
+  it("getEmbeddedScope", () => {
+    const editor = {
+      scopeDescriptorForBufferPosition: () => {
+        return {
+          getScopesArray: () => [
+            "text.md",
+            "fenced.code.md",
+            "source.embedded.python"
+          ]
+        };
+      }
+    };
+    spyOn(editor, "scopeDescriptorForBufferPosition").andCallThrough();
+    const scope = getEmbeddedScope(editor, "position");
+    expect(scope).toEqual("source.embedded.python");
+    expect(editor.scopeDescriptorForBufferPosition).toHaveBeenCalledWith(
+      "position"
+    );
+  });
 });
