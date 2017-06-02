81fed1cf113c4483df83958fd87d87a558263e2f
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-06-02 11:31:57 +0200

    fixed spec-test for getCode() function

--- spec/store/watch-spec.js
+++ spec/store/watch-spec.js
@@ -3,13 +3,13 @@
 import WatchStore from "../../lib/store/watch";
 
 describe("WatchStore", () => {
-  it("class WatchStore", () => {
-    describe("getCode", () => {
-      it("checks for getCode function", () => {
-        //setCode to some string
-        setCode("foo");
-        expect(getCode()).toEqual("foo");
-      });
+  let store;
+  describe("getCode", () => {
+    it("checks for getCode function", () => {
+      store = new WatchStore({})
+      //setCode to some string
+      store.setCode("foo");
+      expect(store.getCode()).toEqual("foo");
     });
   });
 });
