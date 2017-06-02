994ba6e1e6d145e0b2ab402c3f80de9eba58a42d
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-06-02 16:00:22 +0200

    update on watch-specs

--- spec/store/watch-spec.js
+++ spec/store/watch-spec.js
@@ -4,12 +4,41 @@ import WatchStore from "../../lib/store/watch";
 
 describe("WatchStore", () => {
   let store;
+  beforeEach(() => {
+    store = new WatchStore({});
+  });
+  describe("run", () => {
+    it("checks for @action run function", () => {
+      store.setCode("foo");
+      const code = store.getCode();
+      store.kernel = {
+        executeWatch: (code, callback) => callback("result")
+      };
+      spyOn(store.outputStore, "appendOutput");
+      store.run();
+      expect(store.outputStore.appendOutput).toHaveBeenCalled();
+    });
+  });
+  describe("setCode", () => {
+    it("checks for setCode function", () => {
+      //setCode to some string
+      spyOn(store.editor, "setText");
+      store.setCode("foo");
+      expect(store.editor.setText).toHaveBeenCalledWith("foo");
+    });
+  });
   describe("getCode", () => {
     it("checks for getCode function", () => {
-      store = new WatchStore({})
       //setCode to some string
       store.setCode("foo");
       expect(store.getCode()).toEqual("foo");
+    });
+  });
+  describe("focus", () => {
+    it("checks for focus function", () => {
+      spyOn(store.editor.element, "focus");
+      store.focus();
+      expect(store.editor.element.focus).toHaveBeenCalled();
     });
   });
 });
