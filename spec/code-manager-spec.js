6e90837c790eb800291e880dc779dd3c98562882
t9md <taqumd@gmail.com>
2017-06-07 16:27:31 +0900

    add refresh-all-result-bubbles command

--- spec/code-manager-spec.js
+++ spec/code-manager-spec.js
@@ -1,6 +1,7 @@
 "use babel";
 
 import * as CM from "../lib/code-manager";
+import { Point, Range } from "atom";
 
 describe("CodeManager", () => {
   let editor;
@@ -57,6 +58,66 @@ describe("CodeManager", () => {
       spyOn(editor, "getSelectedText");
       CM.getSelectedText(editor);
       expect(editor.getSelectedText).toHaveBeenCalled();
+    });
+  });
+
+  describe("cells", () => {
+    const toPoint = point => Point.fromObject(point);
+    const toRange = range => Range.fromObject(range);
+
+    describe("getCellsForBreakPoints", () => {
+      it("return cell(range) from array of points", () => {
+        const points = [[1, 2], [3, 4], [5, 6], [10, 5]];
+        const cell1 = [[1, 2], [3, 4]];
+        const cell2 = [[4, 0], [5, 6]];
+        const cell3 = [[6, 0], [10, 5]];
+        const cellsExpected = [cell1, cell2, cell3].map(toRange);
+        const cellsActual = CM.getCellsForBreakPoints(points.map(toPoint));
+        expect(cellsActual).toEqual(cellsExpected);
+      });
+    });
+    describe("getCells", () => {
+      // runAsync is borrowed and modified from link below.
+      // https://github.com/jasmine/jasmine/issues/923#issuecomment-169634461
+      function waitAsync(fn) {
+        return done => {
+          fn().then(done, function rejected(e) {
+            fail(e);
+            done();
+          });
+        };
+      }
+      beforeEach(
+        waitAsync(async () => {
+          await atom.packages.activatePackage("language-javascript");
+          const jsGrammar = atom.grammars.grammarForScopeName("source.js");
+          editor.setGrammar(jsGrammar);
+          const code = "var v1 = 1; // %%\nvar v2 = 2;\nvar v2 = 3; // %%\n";
+          editor.setText(code);
+        })
+      );
+      describe("no arg", () => {
+        it("return cell(range) by collecting breakpoints from comments in editor", () => {
+          const cell1 = [[0, 0], [0, 12]];
+          const cell2 = [[1, 0], [2, 12]];
+          const cell3 = [[3, 0], [3, 0]];
+          const cellsActual = CM.getCells(editor);
+          const cellsExpected = [cell1, cell2, cell3].map(toRange);
+          expect(cellsActual).toEqual(cellsExpected);
+        });
+      });
+      describe("with arg(= oldBreakpoints)", () => {
+        it("return cells(range) from exising and detected breakpoints", () => {
+          oldBreakpoints = [[1, 12], [2, 12]];
+          const cell1 = [[0, 0], [0, 12]];
+          const cell2 = [[1, 0], [1, 12]];
+          const cell3 = [[2, 0], [2, 12]];
+          const cell4 = [[3, 0], [3, 0]];
+          const cellsActual = CM.getCells(editor, oldBreakpoints.map(toPoint));
+          const cellsExpected = [cell1, cell2, cell3, cell4].map(toRange);
+          expect(cellsActual).toEqual(cellsExpected);
+        });
+      });
     });
   });
 });
