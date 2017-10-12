eb8efd759f26edf0d5fcbfd69d69f8865501e933
Lukas Geiger <lukas.geiger94@gmail.com>
2017-10-12 18:51:14 -0500

    Test panes

---
+++ spec/panes-spec.js
@@ -0,0 +1,36 @@
+"use babel";
+
+import { CompositeDisposable } from "atom";
+
+const PANES = ["inspector", "kernel-monitor", "output-area", "watches"];
+const utils = require("../lib/utils");
+
+describe("Panes", () => {
+  PANES.map(file => {
+    describe(file, () => {
+      let pane;
+      beforeEach(() => {
+        spyOn(utils, "reactFactory");
+        const Pane = require("../lib/panes/" + file);
+        pane = new Pane();
+      });
+      it("should correctly initialize", () => {
+        expect(pane.element instanceof HTMLElement).toBe(true);
+        expect(pane.disposer instanceof CompositeDisposable).toBe(true);
+        // Be more specific:
+        expect(utils.reactFactory).toHaveBeenCalled();
+        expect(typeof pane.getTitle()).toBe("string");
+        expect(typeof pane.getURI()).toBe("string");
+        expect(typeof pane.getDefaultLocation()).toBe("string");
+        expect(Array.isArray(pane.getAllowedLocations())).toBe(true);
+      });
+      it("should corectly destroy", () => {
+        spyOn(pane.disposer, "dispose");
+        spyOn(pane.element, "remove");
+        pane.destroy();
+        expect(pane.disposer.dispose).toHaveBeenCalled();
+        expect(pane.element.remove).toHaveBeenCalled();
+      });
+    });
+  });
+});
