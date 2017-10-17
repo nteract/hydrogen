cfb0dc73bb630c6c0c51f33962629cd55e17f6c6
Leon Bentrup <leon@leonadi.de>
2017-10-17 17:06:50 +0200

    Remove uneccessary imports in history-spec.js

--- spec/components/result-view/history-spec.js
+++ spec/components/result-view/history-spec.js
@@ -2,12 +2,7 @@
 
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 
-Enzyme.configure({ adapter: new Adapter() });
-
-import store from "../../../lib/store";
-import Kernel from "../../../lib/kernel";
 import History from "../../../lib/components/result-view/history";
 
 describe("History", () => {
