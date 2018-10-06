a8ba323a8bc94530be7cc56fffced87c85626a5c
Hayden Pierce <hayden@haydenpierce.com>
2018-10-06 18:32:35 -0500

    Add a spec for ResultViewComponent

---
+++ spec/components/result-view/result-view-spec.js
@@ -0,0 +1,72 @@
+"use babel";
+
+import React from "react";
+import { shallow } from "enzyme";
+
+import ResultViewComponent from "../../../lib/components/result-view/result-view";
+
+describe("ResultViewComponent", () => {
+  it("should render", () => {
+    const mockStore = {
+      index: 0,
+      outputs: [{}, {}],
+      status: "",
+      executionCount: 0,
+      position: {
+        lineHeight: 0,
+        lineLength: 0,
+        editorWidth: 0,
+        charWidth: 0
+      }
+    };
+
+    const mockKernel = {};
+
+    const destroy = () => {};
+
+    const component = shallow(
+      <ResultViewComponent
+        store={mockStore}
+        kernel={mockKernel}
+        destroy={destroy}
+        showResult={true}
+      />
+    );
+
+    expect(component.type()).not.toBeNull();
+  });
+
+  it("should destroy itself when x is clicked.", () => {
+    const mockStore = {
+      index: 0,
+      outputs: [{}, {}],
+      status: "",
+      executionCount: 0,
+      position: {
+        lineHeight: 0,
+        lineLength: 0,
+        editorWidth: 0,
+        charWidth: 0
+      }
+    };
+
+    const mockKernel = {};
+
+    let destroyInvoked = false;
+    const destroy = () => {
+      destroyInvoked = true;
+    };
+
+    const component = shallow(
+      <ResultViewComponent
+        store={mockStore}
+        kernel={mockKernel}
+        destroy={destroy}
+        showResult={true}
+      />
+    );
+
+    component.find(".icon-x").simulate("click");
+    expect(destroyInvoked).toBe(true);
+  });
+});
