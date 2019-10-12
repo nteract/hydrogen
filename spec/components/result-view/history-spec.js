76885519e27fbff24651ab72d1b792971b7f403b
Ben Russert <benrussert@gmail.com>
2019-10-12 15:35:41 -0600

    Specs for new range slider

--- spec/components/result-view/history-spec.js
+++ spec/components/result-view/history-spec.js
@@ -1,45 +1,38 @@
 "use babel";
 
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 
 import History from "../../../lib/components/result-view/history";
+import OutputStore from "../../../lib/store/output";
 
 describe("History", () => {
+  let fakeOutput, outputStore, component;
   beforeEach(() => {
     atom.config.set(`Hydrogen.wrapOutput`, false);
+    fakeOutput = {
+      output_type: "display_data",
+      data: {
+        "text/html": "<p>This is some HTML that <b>WILL</b> render</p>",
+        "text/plain": "This is some plain text that WILL NOT render"
+      },
+      metadata: {}
+    };
+    outputStore = new OutputStore();
+    outputStore.index = 0;
+    outputStore.outputs = [fakeOutput, fakeOutput];
+    component = mount(<History store={outputStore} />);
   });
 
   it("should render and display the current output", () => {
-    const mockStore = {
-      index: 0,
-      outputs: [{}, {}]
-    };
-
-    const component = shallow(<History store={mockStore} />);
     expect(component.type()).not.toBeNull();
     expect(component.text()).toContain("1/2");
   });
 
   it("should change the index when the arrows are clicked", () => {
-    const mockStore = {
-      index: 0,
-      outputs: [{}, {}],
-      setIndex: v => {
-        mockStore.index = v;
-      },
-      incrementIndex: () => {
-        mockStore.index++;
-      },
-      decrementIndex: () => {
-        mockStore.index--;
-      }
-    };
-
-    const component = shallow(<History store={mockStore} />);
     component.find(".icon-chevron-right").simulate("click");
-    expect(mockStore.index).toBe(1);
+    expect(outputStore.index).toBe(1);
     component.find(".icon-chevron-left").simulate("click");
-    expect(mockStore.index).toBe(0);
+    expect(outputStore.index).toBe(0);
   });
 });
